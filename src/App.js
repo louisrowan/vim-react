4ddb1bd1824606d2fabf1d0d62b12f66bcb11bfa
Louis Rowan <louis.rowan@icloud.com>
2017-03-24 16:30:27 -0700

    made a change

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to t</h2>
 	  <p>new stuff</p>
+	  <h2>more new stuff</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
